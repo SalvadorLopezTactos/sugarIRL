61f3abae278afbb6759457ccabe2fa39edefb92a
Salvador Lopez <salvador.lopez@enterconecta.com>
2018-08-30 09:38:10 -0500

    Se embebe html en nueva pestaña Cotizador

---
+++ custom/clients/base/layouts/cotizador-lysi/cotizador-lysi.php
@@ -0,0 +1,9 @@
+<?php
+    $viewdefs['base']['layout']['cotizador-lysi'] = array(
+        'type' => 'simple',
+        'components' => array(
+            array(
+                'view' => 'cotizador-lysi',
+            ),
+        ),
+    );
